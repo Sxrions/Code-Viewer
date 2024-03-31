25586662b73cf0953546be5817bfd11529836bfc
Jonah PASCAL <jonah.pascal@orange.fr>
2024-03-31 15:40:46 +0200

    no need to explain

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.getElementById('highlight-style').href = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css';
 document.getElementById('theme-selector').addEventListener('change', function() {
     let theme = this.value;
     console.log(theme);
